52f9897ee4e1f3801eb95d7ed91ea52ee2996c9a
cyberfined <cyberfined@protonmail.com>
2020-08-08 20:01:54 +0300

    Fix side-effect analysis. If side effect was in Assign sideEffectAnalysis doesn't catch it, now it does

--- src/Optim/SideEffectAnalysis.hs
+++ src/Optim/SideEffectAnalysis.hs
@@ -14,16 +14,20 @@ runSideEffectAnalysis irst = irst{sideEffectFuncs = se'}
         (se', _) = foldl' analyseFunc (se, nse) $ funDecs irst
 
 analyseFunc :: (S.Set String, S.Set String) -> FunDec -> (S.Set String, S.Set String)
-analyseFunc (se, nse) fd = analyseFunc' False $ map (node (nodes fd)) $ vertices $ cfg fd
-  where analyseFunc' isUndef ((ir:irs):bs) = case ir of
-            Load _ -> seRes
-            Store{} -> seRes
-            RuntimeCall _ -> seRes
-            Call f _ | S.member f se -> seRes
-                     | S.member f nse -> analyseFunc' isUndef (irs:bs)
-                     | otherwise -> analyseFunc' True (irs:bs)
-            _ -> analyseFunc' isUndef (irs:bs)
-          where seRes = (S.insert (name fd) se, nse)
+analyseFunc (se, nse) fd
+  | S.member (name fd) se || S.member (name fd) nse = (se, nse)
+  | otherwise = analyseFunc' False $ map (node (nodes fd)) $ vertices $ cfg fd
+  where analyseFunc' isUndef ((ir:irs):bs) = nextStep ir
+          where nextStep ir = case ir of
+                    Load _ -> seRes
+                    Store{} -> seRes
+                    RuntimeCall (Exit _) -> seRes
+                    Call f _ | S.member f se -> seRes
+                             | S.member f nse -> analyseFunc' isUndef (irs:bs)
+                             | otherwise -> analyseFunc' True (irs:bs)
+                    Assign _ ir -> nextStep ir
+                    _ -> analyseFunc' isUndef (irs:bs)
+                seRes = (S.insert (name fd) se, nse)
         analyseFunc' isUndef (_:bs) = analyseFunc' isUndef bs
         analyseFunc' isUndef _ = if isUndef
                                     then (se, nse)
